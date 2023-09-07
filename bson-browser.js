2bee3a5643179b915c26507ad50025979d70a6b3
subkey <2822448396@qq.com>
2023-09-07 21:34:23 +0800

    faster for firefox

--- bson-browser.js
+++ bson-browser.js
@@ -8,67 +8,117 @@
 
 
 
+
+
   // == generating ==
+  const U32MAX = 2**32;
   var genSpace = 0;
-  var genArrs = {i: 0};
-  var genp = function() {
-    for(let i=0; i<arguments.length; ++i)
-      genArrs[genArrs.i++] = arguments[i];
-    return arguments.length;
+  var genb = new Uint8Array(65536); // gen buffer
+  var geni = 0; // gen index
+
+  /**
+   * require for more 65536 memory for gen buffer
+   */
+  function genExpand() {
+    var b = new Uint8Array(genb.byteLength + 65536);
+    b.set(genb);
+    genb = b;
+  }
+
+  /**
+   * push to generated buffer
+   * @param {...Uint8Array} buffers buffer to push
+   * @returns {void}
+   */
+  function genp() {
+    for(const buf of arguments) {
+      while(geni + buf.byteLength > genb.byteLength) genExpand();
+      genb.set(buf, geni);
+      geni += buf.byteLength;
+    }
   };
 
   /**
-   * generate part of js value
+   * push to buffer with one byte number
+   * @param {Uint8} n
+   * @returns {void}
+   */
+  function genp1(n) {
+    if(geni + 1 > genb.byteLength) genExpand();
+    genb[geni++] = n;
+  }
+
+  /**
+   * generate part of js value and push to buffer
    * @param {Any} o any js value
-   * @returns {Uint8Array}
+   * @returns {Boolean} whether type of `o` is supported
    */
   function genPart(o) {
-    if(o===null) return genp( new Uint8Array([110]) ) // 110==n
+    if(o===null) return genp1(110)||true; // 110==n
 
     // buffer
-    if(o instanceof ArrayBuffer) return genp(new Uint8Array(o));
-    if(o instanceof Uint8Array) return genp(o);
+    if(o instanceof ArrayBuffer) o = new Uint8Array(o);
+    if(o instanceof Uint8Array) {
+      var len = o.byteLength;
+      if(len >= U32MAX) return false;
+      // push length number of data length
+      var byte = 0;
+      while(len>(256**(++byte))); // get length of bytes of content length
+      genp1(byte);
+      // push data length
+      while(--byte>=0) genp1((len>>(byte*8))&0xff);
+      // push data
+      genp(o);
+      return true;
+    }
 
     // array
     if(Array.isArray(o)) {
-      genp(new Uint8Array([91]));
-      var l = genArrs.i;
-      for(let i=0; i<o.length; ++i) 
-        if(genPart(o[i])) genp(new Uint8Array([44]));
-        if(genArrs.i>l) genArrs.i--;
-      return genp(new Uint8Array([93]));
+      genp1(91); // 91[
+      var _geni = geni;
+      for(const v of o) 
+        if(genPart(v)) genp1(44); // 44,
+      if(geni>_geni) geni--; // if nothing available in array, dont lower `geni`.
+      genp1(93); // 93]
+      return true;
     }
 
     switch(typeof o) {
 
-      case "string": 
-        return genp( new Uint8Array([34]), UTF8.enc(o), new Uint8Array([34]) );
+      case "string": {
+        genp1(34); // 34"
+        genp(UTF8.enc(o));
+        genp1(34);
+        return true;
+      }
 
       case "number": 
-        return genp( UTF8.enc(o.toString()) );
+        return genp( UTF8.enc(o.toString()) )||true;
 
       case "boolean": 
-        return genp( new Uint8Array([(o?116:102)]) ); // 116==t, 102==f
+        return genp1(o?116:102)||true; // 116t, 102f
 
       case "object": {
-        genp(new Uint8Array([123])); // 123=={
+        genp1(123); // 123{
+        var _geni = geni;
         var ks = Object.keys(o);
-        var l = genArrs.i;
-        for(let i=0; i<ks.length; ++i) {
-          const k = ks[i];
-          genp(UTF8.enc(k), new Uint8Array([58])); // 58==:
-          if(genPart(o[k])===null) {
-            genArrs.i -= 2;
+        for(const key of ks) {
+          const kbuf = UTF8.enc(key);
+          genp(kbuf);
+          genp1(58); // 58:
+          if(!genPart(o[key])) {
+            geni -= kbuf.byteLength + 1;
             continue;
           }
-          genp(new Uint8Array([44])); // 44==,
+          genp1(44); // 44,
         }
-        if(genArrs.i>l) genArrs.i--;
-        return genp(new Uint8Array([125])); // 125==}
+        if(geni>_geni) geni--;
+        genp1(125); // 125==}
+        return true;
       }
 
     }
-    return null;
+    return false;
   }
 
   /**
@@ -81,17 +131,15 @@
   function gen(obj, space) {
     // init generation
     genSpace = space;
-    genArrs = {i: 0};
+    genb = new Uint8Array(65536);
+    geni = 0;
     genPart(obj);
-    // concat genArrs into buffer
-    var l = 0;
-    for(var i=0; i<genArrs.i; ++i) l += genArrs[i].byteLength;
-    var u = new Uint8Array(l);
-    var o = 0;
-    for(var i=0; i<genArrs.i; ++i) u.set(genArrs[i],o)||(o+=genArrs[i].byteLength);
+    var u = genb.slice(0, geni);
     u.toString = ()=> UTF8.dec(r);
     return u;
   };
+
+
 
 
 
@@ -104,14 +152,18 @@
    * @param {Uint8Array|ArrayBuffer|String} source enumedly typed source
    */
   function parse(source) {
+    // convert source into Uint8Array
     var s;
-    if(typeof source==="string"||source instanceof String) {
-      
-    }
+    if(typeof source==="string"||source instanceof String) 
+      s = UTF8.enc(source);
+    if(source instanceof ArrayBuffer)
+      s = new Uint8Array(source);
+
+    // 
   };
 
   window.BSON = {
-    gen,UTF8,genp,genArrs
+    gen,UTF8,genp,genp1
   }
 
 }()
